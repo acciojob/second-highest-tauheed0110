1c5d93a1d9b727ab98d96abadc1abed97c6cd0a9
tauheed0110 <tauheed0110@gmail.com>
2024-11-02 12:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function secondHeighest(arr){
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
+
+    arr.forEach(element => {
+        if(element > firstMax){
+            secondMax = firstMax;
+            firstMax = element;
+        }else if(element > secondMax && element < firstMax){
+            secondMax = element;
+        }
+    });
+
+    console.log(secondMax);
+}
